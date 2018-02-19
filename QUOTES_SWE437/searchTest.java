0f83f02b44577c069251c1e41c15f39a4985614e
HarmitSingh001 <35312492+HarmitSingh001@users.noreply.github.com>
2018-02-18 20:21:53 -0500

    Add files via upload
    Test files added for Search

--- QUOTES_SWE437/searchTest.java
+++ QUOTES_SWE437/searchTest.java
@@ -4,18 +4,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class searchTest {
-	QuoteList quotes = new QuoteList(); // find a way to fill list with quotes in xml
-	// quotes will be the object we are testing [e.g. quotes.search(validText,
-	// mode)]
+	QuoteSaxParser parser = new QuoteSaxParser("/Users/Harry/Desktop/spr18/swe437/QUOTES_SWE437/quotes.xml");
 
+	QuoteList quotes = parser.getQuoteList();
+	
 	QuoteList returnList = new QuoteList(); // return value
 
 	int mode;
-
-	String validText = "ENTER VALID QUOTE TEXT";
-	String validAuthor = "ENTER VALID AUTHOR";
-	String validAny = "ENTER A VALID AUTHOR OR TEXT";
-	String invalidString = "ENTER INVALID TEXT";
 
 	/*
 	 * Test 1 needs a mode of 0 (search author), and the string should be a valid
@@ -27,34 +22,77 @@ class searchTest {
 	 */
 
 	@Test
+	// test for search author this should pass with true
 	public void testSearch1() {
 		mode = 0;
-		returnList = quotes.search(validText, mode);
+		returnList = quotes.search("Richard Nixon", mode);
 		int size = returnList.getSize();
 		assertTrue(size != 0);
 	}
-
+	
 	@Test
-	public void testSearch2() {
-		mode = 1;
-		returnList = quotes.search(validAuthor, mode);
-		int size = returnList.getSize();
-		assertTrue(size != 0);
-	}
-
-	@Test
-	public void testSearch3() {
-		mode = 2;
-		returnList = quotes.search(validAny, mode);
-		int size = returnList.getSize();
-		assertTrue(size != 0);
-	}
-
-	@Test
-	public void testSearch4() {
+	// test for search author this should pass with false
+	public void testSearch2() { 
 		mode = 0;
-		returnList = quotes.search(invalidString, mode);
+		returnList = quotes.search("HARVEY DENT", mode);
 		int size = returnList.getSize();
 		assertTrue(size == 0);
 	}
+
+	@Test
+	// test for search text this should pass with true
+	public void testSearch3() { 
+		mode = 1;
+		returnList = quotes.search("For every problem there is one solution which is simple, neat, and wrong.", mode);
+		int size = returnList.getSize();
+		assertTrue(size != 0);
+	}
+
+	@Test
+	// test for search text this should pass with false
+	public void testSearch4() { 
+		mode = 1;
+		returnList = quotes.search("HEHEHEHEHEH HAHAHAAHAH HAHAHA", mode);
+		int size = returnList.getSize();
+		assertTrue(size == 0);
+	}
+
+	
+	@Test
+	// test for search general, finding author based, should come back with true
+	public void testSearch5() {
+		mode = 2;
+		returnList = quotes.search("Richard Nixon", mode);
+		int size = returnList.getSize();
+		assertTrue(size != 0);
+	}
+
+	
+	@Test
+	// test for search general, finding text based, should come back with true
+	public void testSearch6() {
+		mode = 2;
+		returnList = quotes.search("Don't tell me how hard you work. Tell me how much you get done.", mode);
+		int size = returnList.getSize();
+		assertTrue(size != 0);
+	}
+	
+	@Test
+	// test for search general, finding text based, should come back with false
+	public void testSearch7() {
+		mode = 2;
+		returnList = quotes.search("Magic is in the air", mode);
+		int size = returnList.getSize();
+		assertTrue(size == 0);
+	}
+	
+	@Test
+	// test for search general, finding author based, should come back with true
+	public void testSearch8() {
+		mode = 2;
+		returnList = quotes.search("mike mickey", mode);
+		int size = returnList.getSize();
+		assertTrue(size == 0);
+	}
+	
 }
